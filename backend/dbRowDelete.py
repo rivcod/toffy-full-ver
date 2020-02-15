b840bd2e59a8f0fc83230ed26c87c0b1fdb823f9
Seono <pianist5555@gmail.com>
2020-02-15 22:24:47 +0900

    0215 크롤링 && DB삭제PY

---
+++ backend/dbRowDelete.py
@@ -0,0 +1,11 @@
+## DB 데이터 삭제 py
+
+import sqlite3
+import django
+import os
+import sys
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+from tfgame.models import Tfgame
+
+Tfgame.objects.filter(company='test').delete()
