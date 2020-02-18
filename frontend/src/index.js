ac80778958eee078a6c510ff2821617278543876
rivcod <game9um@gmail.com>
2020-02-18 22:33:59 +0900

    0218

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 // import App from './App';
-import Root from "/Root";
+import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
