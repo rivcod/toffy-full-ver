9c1a88ec7b562e4304406aaa6eb2607c81d439f1
rivcod <game9um@gmail.com>
2020-02-18 22:41:01 +0900

    0218

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 // import App from './App';
 import Root from "./Root";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 
-serviceWorker();
+// serviceWorker();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 export { default as Home } from "./pages/Home.js";
 export { default as Mypage } from "./pages/MyPage.js";
