b2eaffdf632d5339e3be10519c7a414fb99fee22
rivcod <game9um@gmail.com>
2020-02-19 21:31:04 +0900

    0219

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -10,12 +10,52 @@ import "../css/bootstrap4-neon-glow.css";
 class SignUp extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       email: "",
       username: "",
       password: ""
     };
   }
+
+// 이메일 인풋창 핸들링
+handleEmail = e => {
+  e.preventDfault ();
+  this.setState({
+    email: e.target.value
+  })
+};
+
+// 유저네임 인풋창 핸들링
+handleUsername = e => {
+  e.preventDefault();
+  this.setState({
+    username: e.target.value
+  });
+};
+
+//첫번째 패스워드 입력창 set변환
+handlePW = E => {
+  e.preventDefault();
+  this.setState({
+    pw: e.target.value
+  });
+};
+
+// 서버로 가입 양식 제출
+handleSubmit = e => {
+  e.preventDefault();
+  const {
+    email,
+    username,
+    password
+  } = this.state;
+
+  // const SignUp = {
+  //   email : this.state.emailCheck,
+  //   password: this.state.passwordCheck,
+  //   username: this.state.usernameCheck
+  // };
 
 
   render() {
