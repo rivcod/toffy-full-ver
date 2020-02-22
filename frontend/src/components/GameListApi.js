cfda90717511492135c51f08724688c2ceaf94ab
Seono <pianist5555@gmail.com>
2020-02-22 14:14:03 +0900

    0222 rollback

--- frontend/src/components/GameListApi.js
+++ frontend/src/components/GameListApi.js
@@ -2,80 +2,82 @@ import React, { useState, Fragment } from 'react';
 
 const [count, setCount] = useState(1);
 
-function increment() {
+increment = () => {
   setCount(count + 1);
 }
-function initCount() {
+initCount = () => {
   setCount(0);
 }
 
 const GameListApi = ({ title, company, price, os, release, image, rank}) => {
   return (
-     { count } <= 4 ? 
-      ({increment()}
-        <td>
-            <div className="col-sm-20 GameCard">
-              <a href="/">
-                {image}
-              </a>
-              <div className="featured-job">
-                <div className="title" id="titlePadding">
-                  <h5>{title}</h5>
-                  <p>{company}</p>
-                </div>
-                <div id="_price">
-                  <a
-                    href="/"
-                    className="ht-tm-element btn btn-outline-primary btn-sm"
-                    id="_priceBtn"
-                  >
-                    {price}
-                  </a>
-                </div>
-                <div className="data">
-                  <a className="ht-tm-element badge badge-primary">#어드벤처</a>
-                  <a className="ht-tm-element badge badge-primary">#인디</a>
-                  <a className="ht-tm-element badge badge-primary">#전략</a>
-                </div>
-              </div>
-            </div>
-        </td>
+  //    { count } <= 4 ? 
+  //     (
+  //       {increment()}
+  //       <td>
+  //           <div className="col-sm-20 GameCard">
+  //             <a href="/">
+  //               {image}
+  //             </a>
+  //             <div className="featured-job">
+  //               <div className="title" id="titlePadding">
+  //                 <h5>{title}</h5>
+  //                 <p>{company}</p>
+  //               </div>
+  //               <div id="_price">
+  //                 <a
+  //                   href="/"
+  //                   className="ht-tm-element btn btn-outline-primary btn-sm"
+  //                   id="_priceBtn"
+  //                 >
+  //                   {price}
+  //                 </a>
+  //               </div>
+  //               <div className="data">
+  //                 <a className="ht-tm-element badge badge-primary">#어드벤처</a>
+  //                 <a className="ht-tm-element badge badge-primary">#인디</a>
+  //                 <a className="ht-tm-element badge badge-primary">#전략</a>
+  //               </div>
+  //             </div>
+  //           </div>
+  //       </td>
         
-      ) 
-      : 
-      (
-        <tr>
-          <td>
-              <div className="col-sm-20 GameCard">
-                <a href="/">
-                  {image}
-                </a>
-                <div className="featured-job">
-                  <div className="title" id="titlePadding">
-                    <h5>{title}</h5>
-                    <p>{company}</p>
-                  </div>
-                  <div id="_price">
-                    <a
-                      href="/"
-                      className="ht-tm-element btn btn-outline-primary btn-sm"
-                      id="_priceBtn"
-                    >
-                      {price}
-                    </a>
-                  </div>
-                  <div className="data">
-                    <a className="ht-tm-element badge badge-primary">#어드벤처</a>
-                    <a className="ht-tm-element badge badge-primary">#인디</a>
-                    <a className="ht-tm-element badge badge-primary">#전략</a>
-                  </div>
-                </div>
-              </div>
-            </td>
-        </tr>
-      )
-    
+  //     ) 
+  //     : 
+  //     (
+  //       <tr>
+  //         <td>
+  //             <div className="col-sm-20 GameCard">
+  //               <a href="/">
+  //                 {image}
+  //               </a>
+  //               <div className="featured-job">
+  //                 <div className="title" id="titlePadding">
+  //                   <h5>{title}</h5>
+  //                   <p>{company}</p>
+  //                 </div>
+  //                 <div id="_price">
+  //                   <a
+  //                     href="/"
+  //                     className="ht-tm-element btn btn-outline-primary btn-sm"
+  //                     id="_priceBtn"
+  //                   >
+  //                     {price}
+  //                   </a>
+  //                 </div>
+  //                 <div className="data">
+  //                   <a className="ht-tm-element badge badge-primary">#어드벤처</a>
+  //                   <a className="ht-tm-element badge badge-primary">#인디</a>
+  //                   <a className="ht-tm-element badge badge-primary">#전략</a>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </td>
+  //       </tr>
+  //     )
+  pass
   );
+  
 };
 
 export default GameListApi;
