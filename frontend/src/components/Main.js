a9f027ccf2ea60d957fd92646d1ee686e2e70fbf
Seono <pianist5555@gmail.com>
2020-05-16 19:07:48 +0900

    0516

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,50 +1,90 @@
 import React, { Fragment, useState } from "react";
 
-const Main = () => {
+const askArr = []
 
+const Main = () => {
+  const OX = () => {
+    return (
+      <Fragment>
+      <button onClick = {() => {console.log("Click")}}>
+        O
+      </button>
+      <button onClick = {() => {console.log("Click")}}>
+        X
+      </button>
+      </Fragment>
+    )
+  }
   const quest1 = () => {
     return (
-      "댕댕이보다 냥냥이!"
-    );
+      <Fragment>
+        "댕댕이보다 냥냥이!"
+        {OX()}
+      </Fragment>
+    )
   }
   const quest2 = () => {
     return (
+      <Fragment>
       "나만의 건축물을 설계하고 건설하고 싶다."
+      {OX()}
+      </Fragment>
     );
   }
   const quest3 = () => {
     return (
+      <Fragment>
       "등골이 서늘해지는 짜릿한 공포를 느끼고 싶다."
+      {OX()}
+      </Fragment>
     );
   }
   const quest4 = () => {
     return (
+      <Fragment>
       "현실 연애 스킬, 게임에서 써볼까?"
+      {OX()}
+      </Fragment>
     );
   }
   const quest5 = () => {
     return (
+      <Fragment>
       "귀엽고 아기자기한 그래픽"
+      {OX()}
+      </Fragment>
     );
 }
   const quest6 = () => {
     return (
+      <Fragment>
       "좀비물 완전 내 취향!"
+      {OX()}
+      </Fragment>
     );
   }
   const quest7 = () => {
     return (
+      <Fragment>
       "해결되지 않은 미지의 것에 대한 호기심이 있다."
+      {OX()}
+      </Fragment>
     );
   }
   const quest8 = () => {
     return (
+      <Fragment>
       "어두운 분위기가 좋다."
+      {OX()}
+      </Fragment>
     );
   }
   const quest9 = () => {
     return (
+      <Fragment>
       "영화 속 이야기의 주인공이 돼보고 싶다."
+      {OX()}
+      </Fragment>
     );
   }
 
