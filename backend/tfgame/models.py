1b4614bb77816cbc722a40a008c0f7037f35b574
rivcod <game9um@gmail.com>
2020-05-16 16:03:04 +0900

    0516

--- backend/tfgame/models.py
+++ backend/tfgame/models.py
@@ -15,6 +15,15 @@ class Tfgame(models.Model):
     rank = models.CharField(max_length=30,
                               verbose_name='rank',
                               default='SOME STRING')
+    tag1 = models.CharField(max_length=25,
+                              verbose_name='tag1',
+                              default='SOME STRING')
+    tag2 = models.CharField(max_length=25,
+                              verbose_name='tag2',
+                              default='SOME STRING')
+    tag3 = models.CharField(max_length=25,
+                              verbose_name='tag3',
+                              default='SOME STRING')                                                    
 
     class Meta:
         db_table = 'toffy_game'
