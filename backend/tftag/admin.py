08bd3ea52d0885de2c4b4aef493cb84d324040ab
rivcod <game9um@gmail.com>
2020-05-18 23:20:46 +0900

    0518

--- backend/tftag/admin.py
+++ backend/tftag/admin.py
@@ -4,7 +4,8 @@ from .models import Tftag
 # Register your models here.
 
 class TftagAdmin(admin.ModelAdmin):
-    list_display = ('action', 'arcade', 'indie', 'casual', 'adventure', 'simulation', 'strategy', 'roleplay',
+    list_display = (
+        'num', 'action', 'arcade', 'indie', 'casual', 'adventure', 'simulation', 'strategy', 'roleplay',
     'sports', 'horror', 'racing', 'pixel', 'puzzle', 'story', 'fantasy', 'shooting', 'survival', 'fps',
     'explorer', 'cute', 'movie', 'zombie', 'dark', 'mystery', 'structure', 'management', 'act_adventure',
     'act_roleplay', 'war', 'music', 'rom_simulation', 'post_apocalypse', 'addictive', 'beautiful', 'romance',
