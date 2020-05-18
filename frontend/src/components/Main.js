fc7ef74d4dabd44f634014dbf4bda8c53f903f78
Seono <pianist5555@gmail.com>
2020-05-18 17:36:39 +0900

    0518
    fix

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -10,7 +10,6 @@ const Main = () => {
   const OX = (questNum) => {
     if(questNum =="quest1"){
       return (
-        
         <Fragment>
         <button onClick = {() => {addArr("dog")}}>
           O
@@ -20,15 +19,17 @@ const Main = () => {
         </button>
         </Fragment>
       )
-    } ifelse(questNum =="quest2"){
-      <Fragment>
-        <button onClick = {() => {addArr("constructure")}}>
-          O
-        </button>
-        <button onClick = {() => {addArr("X")}}>
-          X
-        </button>
+    } else if(questNum =="quest2"){
+      return(
+        <Fragment>
+          <button onClick = {() => {addArr("constructure")}}>
+            O
+          </button>
+          <button onClick = {() => {addArr("X")}}>
+            X
+          </button>
         </Fragment>
+      )
     }
     
   }
@@ -42,6 +43,7 @@ const Main = () => {
   }
   const quest2 = () => {
     return (
+      <Fragment>
       "나만의 건축물을 설계하고 건설하고 싶다."
       {OX("quest1")}
       </Fragment>
@@ -49,37 +51,51 @@ const Main = () => {
   }
   const quest3 = () => {
     return (
+      <Fragment>
       "등골이 서늘해지는 짜릿한 공포를 느끼고 싶다."
+      </Fragment>
     );
   }
   const quest4 = () => {
     return (
+      <Fragment>
       "현실 연애 스킬, 게임에서 써볼까?"
+      </Fragment>
     );
   }
   const quest5 = () => {
     return (
+      <Fragment>
       "귀엽고 아기자기한 그래픽"
+      </Fragment>
     );
 }
   const quest6 = () => {
     return (
+      <Fragment>
       "좀비물 완전 내 취향!"
+      </Fragment>
     );
   }
   const quest7 = () => {
     return (
+      <Fragment>
       "해결되지 않은 미지의 것에 대한 호기심이 있다."
+      </Fragment>
     );
   }
   const quest8 = () => {
     return (
+      <Fragment>
       "어두운 분위기가 좋다."
+      </Fragment>
     );
   }
   const quest9 = () => {
     return (
+      <Fragment>
       "영화 속 이야기의 주인공이 돼보고 싶다."
+      </Fragment>
     );
   }
 
