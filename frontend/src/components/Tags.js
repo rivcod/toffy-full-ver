3df04f93c072f921001cba33d64eb967e1fdb4c4
rivcod <game9um@gmail.com>
2020-05-20 20:06:34 +0900

    0520

--- frontend/src/components/Tags.js
+++ frontend/src/components/Tags.js
@@ -6,142 +6,157 @@ class Tags extends Component {
       <div className="ht-tm-codeblock TagsContainer">
         <div className="container py-5 mb5">
           <a href="#!" className="ht-tm-element badge badge-primary">
+            #액션
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #아케이드
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
             #인디
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #스토리텔링
+            #캐주얼
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #어드벤처
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #자연경관이 아름다운
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
             #시뮬레이션
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #판타지
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #공포
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #전략
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #건설
+            #롤플레잉
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #스포츠
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #공포
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #레이싱
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #픽셀 그래픽
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #퍼즐
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #어드벤처
+            #스토리텔링
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #온라인 협동
+            #판타지
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #슈팅
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #생존
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #FPS
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #탐험
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #귀여운
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #비주얼 노벨
+            #영화
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #좀비
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #어두운
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #미스터리
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #건설
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #경영
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
+            #액션 어드벤처
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #액션 롤플레잉
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #전쟁
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #음악
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
             #연애 시뮬레이션
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #로그라이크
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #숨겨진 과제
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #포스트 아포칼립스
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #자연경관이 아름다운
+            #중독성
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #아름다운
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #로맨스
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #초현실적
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #턴제 전투
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
             #마법사
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #수사관
+            #카드게임
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #쌍방향 소설
+            #제2차 세계대전
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #범죄
+            #레이싱
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #스릴러
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #비행
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #시뮬레이션
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #추상적
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #1980년대
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #대전략
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #보드게임
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #킥스타터
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #2.5D
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #내레이션
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #원터치
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #웨스턴
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #던전 앤 드래곤
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #해군
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #암살
-          </a>
-          <a href="#!" className="ht-tm-element badge badge-primary">
-            #사냥
+            #리듬
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #철학적
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #시간 조종
+            #시간여행
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
             #고양이
           </a>
           <a href="#!" className="ht-tm-element badge badge-primary">
-            #미스터리 던전
+            #개
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #축구
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #낚시
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #바이크
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #골프
+          </a>
+          <a href="#!" className="ht-tm-element badge badge-primary">
+            #당구
           </a>
         </div>
       </div>
