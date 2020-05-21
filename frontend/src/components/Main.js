f40096f98b0ab270e9062344743855660be514c9
rivcod <game9um@gmail.com>
2020-05-21 16:44:01 +0900

    0521

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -9,121 +9,190 @@ const Main = () => {
     setAskArr([
       ...askArr,
       {
-        answer
-      }
+        answer,
+      },
     ]);
   };
   // 버튼 함수 시작
   const OX = (questNum) => {
-    if(questNum =="quest1"){
-      console.log("로그1"+askArr)
+    if (questNum === "quest1") {
+      console.log("로그1" + askArr);
       return (
         <Fragment>
-        <button onClick = {() => {addArr("dog")}}>
-          O
-        </button>
-        <button onClick = {() => {addArr("cat")}}>
-          X
-        </button>
-        </Fragment>
-      )
-    } else if(questNum =="quest2"){
-      console.log("로그2"+askArr)
-      return(
-        <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("dog");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("cat");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest3"){
-      console.log("로그3"+askArr)
-      return(
+      );
+    } else if (questNum === "quest2") {
+      console.log("로그2" + askArr);
+      return (
         <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest4"){
-      return(
+      );
+    } else if (questNum === "quest3") {
+      console.log("로그3" + askArr);
+      return (
         <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest5"){
-      return(
+      );
+    } else if (questNum === "quest4") {
+      return (
         <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest6"){
-      return(
+      );
+    } else if (questNum === "quest5") {
+      return (
         <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest7"){
-      return(
+      );
+    } else if (questNum === "quest6") {
+      return (
         <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest8"){
-      return(
+      );
+    } else if (questNum === "quest7") {
+      return (
         <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest9"){
-      return(
+      );
+    } else if (questNum === "quest8") {
+      return (
         <Fragment>
-          <button onClick = {() => {addArr("constructure")}}>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
             O
           </button>
-          <button onClick = {() => {addArr("X")}}>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
             X
           </button>
         </Fragment>
-      )
-    } else if(questNum =="quest10"){
-      return(
-        addArr("done")
-      )
+      );
+    } else if (questNum === "quest9") {
+      return (
+        <Fragment>
+          <button
+            onClick={() => {
+              addArr("constructure");
+            }}
+          >
+            O
+          </button>
+          <button
+            onClick={() => {
+              addArr("X");
+            }}
+          >
+            X
+          </button>
+        </Fragment>
+      );
+    } else if (questNum === "quest10") {
+      return addArr("done");
     }
-    
-  }
+  };
   // 질문함수 시작
   const quest1 = () => {
     return (
@@ -131,104 +200,104 @@ const Main = () => {
         "댕댕이보다 냥냥이!"
         {OX("quest1")}
       </Fragment>
-    )
-  }
+    );
+  };
   const quest2 = () => {
     return (
       <Fragment>
-      "나만의 건축물을 설계하고 건설하고 싶다."
-      {OX("quest2")}
+        "나만의 건축물을 설계하고 건설하고 싶다."
+        {OX("quest2")}
       </Fragment>
     );
-  }
+  };
   const quest3 = () => {
     return (
       <Fragment>
-      "등골이 서늘해지는 짜릿한 공포를 느끼고 싶다."
-      {OX("quest3")}
+        "등골이 서늘해지는 짜릿한 공포를 느끼고 싶다."
+        {OX("quest3")}
       </Fragment>
     );
-  }
+  };
   const quest4 = () => {
     return (
       <Fragment>
-      "현실 연애 스킬, 게임에서 써볼까?"
-      {OX("quest4")}
+        "현실 연애 스킬, 게임에서 써볼까?"
+        {OX("quest4")}
       </Fragment>
     );
-  }
+  };
   const quest5 = () => {
     return (
       <Fragment>
-      "귀엽고 아기자기한 그래픽"
-      {OX("quest5")}
+        "귀엽고 아기자기한 그래픽"
+        {OX("quest5")}
       </Fragment>
     );
-}
+  };
   const quest6 = () => {
     return (
       <Fragment>
-      "좀비물 완전 내 취향!"
-      {OX("quest6")}
+        "좀비물 완전 내 취향!"
+        {OX("quest6")}
       </Fragment>
     );
-  }
+  };
   const quest7 = () => {
     return (
       <Fragment>
-      "해결되지 않은 미지의 것에 대한 호기심이 있다."
-      {OX("quest7")}
+        "해결되지 않은 미지의 것에 대한 호기심이 있다."
+        {OX("quest7")}
       </Fragment>
     );
-  }
+  };
   const quest8 = () => {
     return (
       <Fragment>
-      "어두운 분위기가 좋다."
-      {OX("quest8")}
+        "어두운 분위기가 좋다."
+        {OX("quest8")}
       </Fragment>
     );
-  }
+  };
   const quest9 = () => {
     return (
       <Fragment>
-      "영화 속 이야기의 주인공이 돼보고 싶다."
-      {OX("quest9")}
+        "영화 속 이야기의 주인공이 돼보고 싶다."
+        {OX("quest9")}
       </Fragment>
     );
-  }
+  };
   const greet = () => {
     return (
       <Fragment>
-      "축하합니다 설문조사를 완료했습니다. 블라블라"
-      {OX("quest10")}
+        "축하합니다 설문조사를 완료했습니다. 블라블라"
+        {OX("quest10")}
       </Fragment>
     );
-  }
+  };
 
   const branchRendering = (num) => {
-    if(!num[0]){
-      return quest1()
-    } else if(num[0]){
-      return quest2()
-    } else if(num[1]){
-      return quest3()
-    } else if(num[2]){
-      return quest4()
-    } else if(num[3]){
-      return quest5()
-    } else if(num[4]){
-      return quest6()
-    } else if(num[5]){
-      return quest7()
-    } else if(num[6]){
-      return quest8()
-    } else if(num[7]){
-      return quest9()
-    } else if(num[8]){
-      return greet()
+    if (!num[0]) {
+      return quest1();
+    } else if (num[0]) {
+      return quest2();
+    } else if (num[1]) {
+      return quest3();
+    } else if (num[2]) {
+      return quest4();
+    } else if (num[3]) {
+      return quest5();
+    } else if (num[4]) {
+      return quest6();
+    } else if (num[5]) {
+      return quest7();
+    } else if (num[6]) {
+      return quest8();
+    } else if (num[7]) {
+      return quest9();
+    } else if (num[8]) {
+      return greet();
     }
-  }
+  };
 
   return (
     <div id="ht-tm-jumbotron">
@@ -245,9 +314,7 @@ const Main = () => {
                 </div>
 
                 <p class="mt-5 text-grey text-spacey ask">
-                  {
-                    branchRendering(askArr)
-                  }
+                  {branchRendering(askArr)}
                 </p>
 
                 <div class="text-mono">
@@ -265,6 +332,6 @@ const Main = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Main;
