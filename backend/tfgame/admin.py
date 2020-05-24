9ccee7b29cf01483f7180fa39d264af29dc841fb
rivcod <game9um@gmail.com>
2020-05-24 15:45:32 +0900

    0524

--- backend/tfgame/admin.py
+++ backend/tfgame/admin.py
@@ -3,7 +3,10 @@ from .models import Tfgame
 
 # Register your models here.
 
+
 class TfgameAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'price', 'href', 'image', 'rank', 'tag1', 'tag2', 'tag3')
+    list_display = ('rank', 'id', 'title', 'price', 'tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'href', 'image'
+                    )
+
 
 admin.site.register(Tfgame, TfgameAdmin)
--- backend/tfgame/models.py
+++ backend/tfgame/models.py
@@ -2,30 +2,37 @@ from django.db import models
 
 # Create your models here.
 
+
 class Tfgame(models.Model):
     id = models.AutoField(primary_key=True)
 
     title = models.CharField(max_length=50,
                              verbose_name='title')
     price = models.CharField(max_length=7,
-                              verbose_name='price')
+                             verbose_name='price')
     href = models.CharField(max_length=300,
-                              verbose_name='href')                                                  
+                            verbose_name='href')
     image = models.CharField(max_length=300,
-                              verbose_name='image',
-                              default='SOME STRING')
+                             verbose_name='image',
+                             default='SOME STRING')
     rank = models.CharField(max_length=4,
-                              verbose_name='rank')
+                            verbose_name='rank')
     tag1 = models.CharField(max_length=2,
-                              verbose_name='tag1',
-                              default='0')
+                            verbose_name='tag1',
+                            default='0')
     tag2 = models.CharField(max_length=2,
-                              verbose_name='tag2',
-                              default='0')
+                            verbose_name='tag2',
+                            default='0')
     tag3 = models.CharField(max_length=2,
-                              verbose_name='tag3',
-                              default='0')
-                                                                                  
+                            verbose_name='tag3',
+                            default='0')
+    tag4 = models.CharField(max_length=2,
+                            verbose_name='tag4',
+                            default='0')
+    tag5 = models.CharField(max_length=2,
+                            verbose_name='tag5',
+                            default='0')
+
     class Meta:
         db_table = 'toffy_game'
         verbose_name = '게임'
