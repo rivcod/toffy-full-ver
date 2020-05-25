107c60f94dbe49e45343a275be3320b06e2177f1
Seono <pianist5555@gmail.com>
2020-05-25 14:55:30 +0900

    0524 We don't need img files

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,108 +1,115 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 
 import "../css/bootstrap4-neon-glow.css";
 
-const MyPage = () => {
-  const [contact, setContact] = useState({
-    writer: [],
-    text: [],
-    date: []
-  });
-
-  useEffect(() => {
-    _renderText();
-  },[contact.text]);
-
+class MyPage extends Component {
+  state = {
+    value: "",
+    textList: []
+  };
+  componentDidMount() {
+    this._renderText();
+  }
   //map new object 생성
-  const _writer = contact.writer.map()
-  console.log("확인 ===="+contact.writer[1])
-  console.log("확인2 ===="+_writer)
+  //console.log("확인 ===="+contact.writer[1])
+  //console.log("확인2 ===="+_writer)
 
   // 텍스트 렌더링
-  const _renderText = () => {
+  _renderText = () => {
     axios
-    // Restframework에서 json으로 보여주는 페이지 url을 get 방식으로 접근
+      // Restframework에서 json으로 보여주는 페이지 url을 get 방식으로 접근
       .get("/api/tfnote/")
-      .then(res => setContact({ writer: res.data})) 
+      .then(res => this.setState({ textList: res.data }))
       .catch(err => console.log(err));
+
+    console.log("들어왔는지 볼까? " + this.state.textList)
   };
 
 
   // writer
-  const handleEmail = e => {
+  handleEmail = e => {
     e.preventDefault();
-    setContact({
+    this.setState({
       writer: e.target.value
     });
   };
 
   //text
-  const handleUsername = e => {
+  handleUsername = e => {
     e.preventDefault();
-    setContact({
+    this.setState({
       text: e.target.value
     });
   };
 
   //date
-  const handlePassword = e => {
+  handlePassword = e => {
     e.preventDefault();
-    setContact({
+    this.setState({
       date: e.target.value
     });
   };
 
   //서버로 가입 양식 제출
-  const handleSubmit = e => {
+  handleSubmit = e => {
     e.preventDefault();
 
   };
-
-  return (
-    <div className="SignUp">
-      <div className="card-body">
-        <div className="submitForm">
-          <div className="ht-tm-codeblock ht-tm-btn-replaceable ht-tm-element ht-tm-element-inner">
-            <div>
-              {}
+  render() {
+    const { textList } = this.state;
+    return (
+      <div className="SignUp">
+        <div className="card-body">
+          <div className="submitForm">
+            <div className="ht-tm-codeblock ht-tm-btn-replaceable ht-tm-element ht-tm-element-inner">
+              <div>
+                {}
+              </div>
+              <div>
+                {textList.map((text, index) => {
+                  return(
+                  <div className="Contact">
+                  <p>번호 : {index}</p>
+                  <p>작성자 : {text.writer}</p>
+                  <p>본문 : {text.text}</p>
+                  </div>
+                  );
+                })}
+              </div>
+              <div class="submitLogo">
+              </div>
+              <div className="form-group">
+                <input
+                  type="writer"
+                  className="form-control writer"
+                  id="_writer"
+                  aria-describedby="_writer"
+                  placeholder="작성자"
+                ></input>
+                <textarea
+                  type="text"
+                  className="form-control text"
+                  id="_text"
+                  aria-describedby="_text"
+                  placeholder="내용"
+                ></textarea>
+              </div>
             </div>
-            <div>
-              블라블라
-              블라블라
-              블라블라
-            </div>
-            <div class="submitLogo">
-            </div>
-            <div className="form-group">
-              <input
-                type="writer"
-                className="form-control writer"
-                id="_writer"
-                aria-describedby="_writer"
-                placeholder="작성자"
-              ></input>
-              <textarea
-                type="text"
-                className="form-control text"
-                id="_text"
-                aria-describedby="_text"
-                placeholder="내용"
-              ></textarea>
-            </div>
-          </div>
-          <div id="_apiJoinclassName">
-            <a href="#!" class="ht-tm-element btn btn-shadow text-mono btn-warning">
-              <span class="fa fa-download mr-2"></span>글쓰기
+            <div id="_apiJoinclassName">
+              <a href="#!" class="ht-tm-element btn btn-shadow text-mono btn-warning">
+                <span class="fa fa-download mr-2"></span>글쓰기
           </a>
-          </div>
-          {/* <button onClick={this.handleSubmit} className="btn btn-primary _joinclassName">
+            </div>
+            {/* <button onClick={this.handleSubmit} className="btn btn-primary _joinclassName">
           Join
         </button> */}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default MyPage;
