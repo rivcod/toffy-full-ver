ce1581922a8bd06dc49dec0008eaf0c09bfe334c
Seono <pianist5555@gmail.com>
2020-06-02 04:01:51 +0900

    0602

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React,{ useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/logo-white1.png";
 
  const Navbar = ({selectedGame, selectGame}) => {
-  var nav = document.getElementsByClassName("_floating");
+  const para = document.location.href.split("/")[3];
+  useEffect(() => {
+    if(para == "Contact"){
+      const myDiv = document.getElementById("inputBlank");
+      myDiv.setAttribute('readonly',true)
+      myDiv.setAttribute('placeholder',"Search is not available here.")
+      console.log("myDiv = "+myDiv.value)
+    }
+  });
+  
+
+  let nav = document.getElementsByClassName("_floating");
   window.onscroll = function sticky() {
     if(window.pageYOffset > nav[0].offsetTop) {
       nav[0].classList.add("navCustom");
@@ -24,7 +35,6 @@ import logo from "../images/logo-white1.png";
     }
     
   };
-
 
   return (
     <div className="navbar-dark text-white _floating">
@@ -53,6 +63,7 @@ import logo from "../images/logo-white1.png";
                 className="form-control _searchFormControl"
                 name="gameName"
                 placeholder="Search"
+                id="inputBlank"
                 onChange={handleChange}
               ></input>
               <div className="_mainSignUp">
