f1dc876f9300c756a35c311f8904afb9e306531d
Seono <pianist5555@gmail.com>
2020-06-01 22:13:48 +0900

    0601

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -5,19 +5,30 @@ const Pagination = ({postsPerPage, totalPosts, currentPage,setCurrentPage}) => {
       for (let i = 1; i <= Math.ceil(totalPosts / 20); i++) {
         pageNumbers.push(i);
       }
-      return (
-        <nav className="paginationNav">
-          <ul className='pagination'>
-            {pageNumbers.map(number => (
-              <li key={number} className='page-item'>
-                <button onClick={() => setCurrentPage(number)} className='page-link'>
-                  {number}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      );
+      const unnecessaryPage = () => {
+        return ""
+      }
+      const necessaryPage = () => {
+        return(
+          <nav className="paginationNav">
+            <ul className='pagination'>
+              {pageNumbers.map(number => (
+                <li key={number} className='page-item'>
+                  <button onClick={() => setCurrentPage(number)} className='page-link'>
+                    {number}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )
+      }
+
+      if(pageNumbers[1]) {
+        return necessaryPage()
+      } else {
+        return unnecessaryPage()
+      }
   }
 
 export default Pagination;
